e9b7b2d222cb482f0cf2a03141ab35ac26981845
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-27 15:21:12 -0400

    added precipitation

--- src/main/java/com/example/demo/business/entities/Category.java
+++ src/main/java/com/example/demo/business/entities/Category.java
@@ -10,35 +10,25 @@ import static javax.persistence.CascadeType.ALL;
 
 @Entity
 public class Category {
-    @OneToMany(cascade = ALL, mappedBy = "category")
-    public Set<Item> items;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     @NotEmpty
-    @Size(min = 4)
-    private String top;
-    @NotEmpty
-    @Size(min = 4)
-    private String bottom;
-    @NotEmpty
-    @Size(min = 4)
-    private String Jacket;
-    @NotEmpty
-    @Size(min = 4)
-    private String shoe;
+    private String name;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public Set<Item> items;
 
     public Category() {
         items = new HashSet<>();
     }
 
-    public Category(@NotEmpty @Size(min = 4) String top, @NotEmpty @Size(min = 4) String bottom, @NotEmpty @Size(min = 4) String jacket, @NotEmpty @Size(min = 4) String shoe) {
-        this.top = top;
-        this.bottom = bottom;
-        Jacket = jacket;
-        this.shoe = shoe;
+    public Category(@NotEmpty String name) {
+        this();
+        this.name = name;
     }
-
 
     public long getId() {
         return id;
@@ -48,36 +38,12 @@ public class Category {
         this.id = id;
     }
 
-    public String getTop() {
-        return top;
+    public String getName() {
+        return name;
     }
 
-    public void setTop(String top) {
-        this.top = top;
-    }
-
-    public String getBottom() {
-        return bottom;
-    }
-
-    public void setBottom(String bottom) {
-        this.bottom = bottom;
-    }
-
-    public String getJacket() {
-        return Jacket;
-    }
-
-    public void setJacket(String jacket) {
-        Jacket = jacket;
-    }
-
-    public String getShoe() {
-        return shoe;
-    }
-
-    public void setShoe(String shoe) {
-        this.shoe = shoe;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Set<Item> getItems() {
--- src/main/java/com/example/demo/business/entities/repositories/SeasonRepository.java
+++ src/main/java/com/example/demo/business/entities/repositories/SeasonRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.business.entities.repositories;
 import com.example.demo.business.entities.Season;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Iterator;
+
 public interface SeasonRepository extends CrudRepository<Season, Long> {
+    Season findByName(String name);
 }
