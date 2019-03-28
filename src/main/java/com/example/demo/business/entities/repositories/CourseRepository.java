7d6c794af43b4a92bc294e03b9ff5914eb88ba78
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-28 17:00:46 -0400

    before merge

--- src/main/java/com/example/demo/business/entities/repositories/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.business.entities.repositories;
-
-import com.example.demo.business.entities.Course;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Course, Long> {
-}
--- src/main/java/com/example/demo/web/controller/HomeController.java
+++ src/main/java/com/example/demo/web/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.demo.web.controller;
 
 import com.cloudinary.utils.ObjectUtils;
-import com.example.demo.business.entities.Course;
 import com.example.demo.business.entities.Item;
 import com.example.demo.business.entities.User;
 import com.example.demo.business.entities.repositories.*;
