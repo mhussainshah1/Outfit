7a1e8261f64601f84e6d5c723a5d99b748a0e119
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-29 13:39:22 -0400

    Data Loader

--- src/main/java/com/example/demo/business/services/DataLoader.java
+++ src/main/java/com/example/demo/business/services/DataLoader.java
@@ -82,6 +82,9 @@ public class DataLoader implements CommandLineRunner {
         categoryRepository.save(new Category("Shoes"));
         Category shoe = categoryRepository.findByName("Shoes");
 
+        categoryRepository.save(new Category("Accessories"));
+        Category accessories = categoryRepository.findByName("Accessories");
+
         //Climate
         climateRepository.save(new Climate("Cold"));
         Climate cold = climateRepository.findByName("Cold");
@@ -111,7 +114,7 @@ public class DataLoader implements CommandLineRunner {
         occasionRepository.save(new Occasion("Dinner"));
         Occasion dinner = occasionRepository.findByName("Dinner");
 
-        //moe items
+       /* //moe items
         itemRepository.save(new Item("shirt",
                 "white",
                 "cotton",
@@ -167,7 +170,164 @@ public class DataLoader implements CommandLineRunner {
 
         //dag item
 
+        //nan item*/
+        //moe items
+        itemRepository.save(new Item("shirt",
+                "grey",
+                "cotton",
+                "small",
+                "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553799014/Outfit/JESUS_IS_MY_HOMEBOY_heathergrey_2000x.jpg",
+                "men's wear",
+                moe,
+                top,
+                cold,
+                dinner));
+        Item Homeboyshirt = itemRepository.findByName("Homeboyshirt");
+
+        itemRepository.save(new Item("cargo pants",
+                        "khaki",
+                        "polyester",
+                        "medium",
+                        "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553802838/Outfit/Cargo_khaki_1.jpg",
+                        "men's wear",
+                        moe,
+                        bottom,
+                        cold,
+                        casual));
+        Item pant = itemRepository.findByName("cargo pant");
+
+        itemRepository.save(
+                new Item("light jacket",
+                        "white",
+                        "leather",
+                        "large",
+                        "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553756501/outfit/Jackets/Warm/Black_Nike_WIndbreaker.jpg",
+                        "men's wear",
+                        moe,
+                        jacket,
+                        mild,
+                        casual));
+        Item lightJacket = itemRepository.findByName("light jacket");
+
+        itemRepository.save(new Item("socks",
+                        "grey",
+                        "cotton",
+                        "US 10",
+                        "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553756502/outfit/Shoes/Warm/Warm_Midhigh_Socks.jpg",
+                        "men's wear",
+                        moe,
+                        shoe,
+                        hot,
+                        casual));
+        Item sandle = itemRepository.findByName("socks");
+
+        //tolani item
+        itemRepository.save(new Item("long Sleeve Shirt",
+                "grey",
+                "cotton",
+                "small",
+                "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553756502/outfit/Tops/Cold/Grey_Long_Sleeve.jpg",
+                "men's wear",
+                tolani,
+                top,
+                cold,
+                dinner));
+        Item shirt = itemRepository.findByName("long Sleeve shirt");
+
+        itemRepository.save(new Item("jeans",
+                        "blue",
+                        "cotton",
+                        "medium",
+                        "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553756501/outfit/Bottom/Cold/Jeans_Blue_1.jpg",
+                        "men's wear",
+                        tolani,
+                        bottom,
+                        cold,
+                        casual));
+        Item jeans = itemRepository.findByName("jeans");
+
+        itemRepository.save(
+                new Item("White and Blue Windbreaker jacket",
+                        "white",
+                        "nylon",
+                        "large",
+                        "https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553756501/outfit/Jackets/Warm/Black_Nike_WIndbreaker.jpg",
+                        "men's wear",
+                        tolani,
+                        jacket,
+                        mild,
+                        casual));
+        Item bluewhiteJacket = itemRepository.findByName("Blue & White jacket");
+
+        itemRepository.save(
+                new Item("NikeSneakers",
+                        "Black and Green",
+                        "leather",
+                        "US 10",
+                        "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553797544/Outfit/Nike-Outdoor-Green-Court-Borough-Mid-Winter-Shoes.jpg",
+                        "men's wear",
+                        tolani,
+                        shoe,
+                        hot,
+                        casual));
+        Item sneakers = itemRepository.findByName("Nike_Sneakers");
+
         //nan item
+        itemRepository.save(
+                new Item("short sleeve Shirt",
+                        "BluePi",
+                        "cotton",
+                        "Medium",
+                        "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553797471/Outfit/Blue_shortsleeve_Science_Shirt.jpg",
+                        "men's wear",
+                        nan,
+                        top,
+                        cold,
+                        dinner));
+        Item pishirt = itemRepository.findByName("shirt");
+
+        itemRepository.save(
+                new Item("khaki shorts",
+                        "Tan",
+                        "cotton",
+                        "medium",
+                        "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553797908/Outfit/Shorts_Khaki_1.jpg",
+                        "men's wear",
+                        nan,
+                        bottom,
+                        cold,
+                        casual));
+        Item khakishorts = itemRepository.findByName("jeans");
+
+        itemRepository.save(
+                new Item("Green and Grey Windbreaker jacket",
+                        "Green Grey",
+                        "nylon",
+                        "large",
+                        "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553797638/Outfit/Green_Grey_1.jpg",
+                        "men's wear",
+                        nan,
+                        jacket,
+                        mild,
+                        casual));
+        Item greengreyJacket = itemRepository.findByName("Green & Grey jacket");
+
+        itemRepository.save(
+                new Item("All weather sandles",
+                        "brown and Green",
+                        "leather",
+                        "US 10",
+                        "https://res.cloudinary.com/toyefule/image/upload/c_fill,g_face,h_150,r_50,w_150/v1553797718/Outfit/Nhan_All-weather_Sandals.jpg",
+                        "men's wear",
+                        nan,
+                        shoe,
+                        cold,
+                        casual));
+        Item sandals = itemRepository.findByName("All weather sandles");
+
+        //dag item
+
+        //mel item
 
        /* Item sandles = new Item("white",
                 "leather",
@@ -181,24 +341,5 @@ public class DataLoader implements CommandLineRunner {
 
         cold.setItem(sandles);
         climateRepository.save(cold);*/
-
-       /*
-        categoryRepository.save(category);
-
-        Car car1 = new Car("Honda","Accord",2019,"35 miles/gallon",45000.55,"https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1552081999/java-bootcamp/bo1q7fwi8qytkxi6yyus.jpg",category);
-        category.getCars().add(car1);
-        carRepository.save(car1);
-
-        Car car2 = new Car("Toyota","Camry",2018,"35 miles/gallon",32000.02,"https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1552100700/java-bootcamp/mj2tywvdhsq0mleow3rp.png",category);
-        category.getCars().add(car2);
-        carRepository.save(car2);*/
-
-        /*
-        categoryRepository.save(category);
-
-        car = new Car("Mercedez","Benz",2019,"35 miles/gallon", 50000.85,"https://res.cloudinary.com/mhussainshah1/image/upload/c_fill,g_face,h_150,r_50,w_150/v1552162481/java-bootcamp/klztzsqldrhak8lvs7zu.png",category);
-        category.getCars().add(car);
-        carRepository.save(car);
-*/
     }
 }
--- src/main/java/com/example/demo/web/controller/WeatherController.java
+++ src/main/java/com/example/demo/web/controller/WeatherController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.Random;
 
 @Controller
 public class WeatherController {
@@ -51,7 +52,58 @@ public class WeatherController {
             throws IOException {
         findAll(model);
         model.addAttribute("weatherData", weatherService.getWeather(formAttributes));
+
         return "weatherDetails";
     }
 
+    public void randomGenerator() {
+        // Initialization of random numbers specific to burrito options
+
+        Random a = new Random();
+        Random b = new Random();
+        Random j = new Random();
+        Random s = new Random();
+        Random t = new Random();
+
+        //String array of Options
+        String[] tops = {"White Short-sleeve T-Shirt, ", "Grey Short-sleeve Sports Shirt, ", "Blue T-Shirt, ", "Blue long Sleeve T-shirt, ", "Blue Short-sleeve Science Shirt, ", "Grey Long Sleeve, "};
+        String[] bottoms = {"Cargo khaki, ", "Jeans Blue, ", "Khaki Brown, ", "Snow Gray, ", "Shorts Blue, ", "Shorts Grey,", "Shorts Khaki"};
+        String[] shoes = {"Black Tan Timberland", "Grey long Socks", "Nike-Green-Winter-Shoes", "Winter-Socks-Grey-White", "Winter-Socks-Grey-Yellow", "Nhan's All-weather Sandals", "Warm Midhigh Socks"};
+        String[] jackets = {"Black Dark-Grey Jacket", "Black Grey Jacket", "Black Light-Grey Jacket", "Black Jacket", "Green Grey Jacket", "Northface Black Jacket", "Black Nike Windbreaker", "White Navy-Blue Windbreaker Columbia"};
+        String[] accessories = {"Winter", "Spring", "Summer", "Fall"};
+
+        // Int list of bounds equal to the length of the attribute list to burrito options
+
+        int alength = accessories.length;
+        int blength = bottoms.length;
+        int jlength = jackets.length;
+        int slength = shoes.length;
+        int tlength = tops.length;
+
+        // Int list of random variables specific to burrito options
+        int ax = a.nextInt(alength + 2);
+        int bx = b.nextInt(blength + 3);
+        int jx = j.nextInt(jlength + 2);
+        int sx = s.nextInt(slength + 2);
+        int tx = t.nextInt(tlength + 2);
+
+// if (item.temp = hot){
+
+
+// }
+        for (int i = 1; i < 8; i++) {
+            // int j = i + 1;
+            System.out.println();
+            System.out.print("Outfit " + i + ": ");
+            System.out.print(tops[t.nextInt(6)]);
+            System.out.print(bottoms[b.nextInt(7)] + " ");
+            System.out.print(shoes[s.nextInt(3)] + ", ");
+            System.out.print(jackets[j.nextInt(8)] + ", ");
+            System.out.print(accessories[a.nextInt(4)] + " ");
+            System.out.println();
+//                System.out.println();
+            /* Tried to print an Array of Arrays, didn't like that one, next I'd attempt an array list of arrays, but the above code will do*/
+            // System.out.println("Burrito "+ i + ": "+ rice[rx] +" "+ meat[mx]+"," + beans[bx]);
+        }
+    }
 }
