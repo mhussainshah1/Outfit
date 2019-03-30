82862189611237781ef7f1ecdb8b9b07e9cba57c
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-29 23:33:16 -0400

    wind

--- src/main/java/com/example/demo/web/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/web/config/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/css/**", "/js/**", "/img/**",
                         "/detail/{id}", "/detailcategory/{id}",
                         "/detailclimate/{id}","/detailoccasion/{id}",
-                        "/about").permitAll()
+                        "/detailwind/{id}","/about").permitAll()
 //                .access("hasAnyAuthority('USER','ADMIN')")
                 .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
