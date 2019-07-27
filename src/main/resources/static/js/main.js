ea64c506978f6dc47f69495a1356d76ed2c681c6
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-07-27 15:28:56 -0400

    Modal delete, Update Group

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
-    $('.delBtn, .table .delBtn').on('click', function(event){
+$(document).ready(function () {
+    $('.delBtn, .table .delBtn').on('click', function (event) {
         event.preventDefault();
         var href = $(this).attr('href');
-        $('#myModal #delRef').attr('href',href);
+        $('#myModal #delRef').attr('href', href);
         $('#myModal').modal();
     });
 });
