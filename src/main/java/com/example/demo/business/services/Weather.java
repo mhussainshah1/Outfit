101076fc43c65588b24e52425744f3ba1cc77e56
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-02-08 03:23:10 -0500

    Java 15, Spring 2.5.0

--- src/main/java/com/example/demo/business/services/Weather.java
+++ src/main/java/com/example/demo/business/services/Weather.java
@@ -1,35 +1,21 @@
 package com.example.demo.business.services;
 
-/**
- * [Source: (String)
- * <p>
- * "{
- * <p>
- * "coord":{"lon":-73.99,"lat":40.73},
- * <p>
- * "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],
- * <p>
- * "base":"stations",
- * <p>
- * "main":{"temp":275.41,"pressure":1020,"humidity":29,"temp_min":273.15,"temp_max":277.15},
- * <p>
- * "visibility":16093,
- * <p>
- * "wind":{"speed":3.6,"deg":360},
- * <p>
- * "clouds":{"all":1},
- * <p>
- * "dt":1553583986,
- * <p>
- * "sys":{"type":1,"id":6015,"message":0.0093,"country":"US","sunrise":1553597383,"sunset":1553642009},
- * <p>
- * "id":5128581,
- * <p>
- * "name":"New York",
- * <p>
- * "cod":200
- * <p>
- * }"
+/*
+  [Source: (String)
+  "{
+      "coord":{"lon":-73.99,"lat":40.73},
+      "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],
+      "base":"stations",
+      "main":{"temp":275.41,"pressure":1020,"humidity":29,"temp_min":273.15,"temp_max":277.15},
+      "visibility":16093,
+      "wind":{"speed":3.6,"deg":360},
+      "clouds":{"all":1},
+      "dt":1553583986,
+      "sys":{"type":1,"id":6015,"message":0.0093,"country":"US","sunrise":1553597383,"sunset":1553642009},
+      "id":5128581,
+      "name":"New York",
+      "cod":200
+  }"
  */
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -37,6 +23,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import org.springframework.context.annotation.Bean;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
@@ -44,6 +31,7 @@ import java.util.Map;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Weather implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 7406210628182440902L;
 
     @JsonProperty("name")
