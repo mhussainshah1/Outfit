9b798d4d2a3a1c1ca9d68938559f0d38ce4ce1c0
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2023-03-21 22:11:33 -0400

    base.html

--- Application/src/main/java/com/outfit/web/config/SecurityConfiguration.java
+++ Application/src/main/java/com/outfit/web/config/SecurityConfiguration.java
@@ -65,7 +65,7 @@ public class SecurityConfiguration {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().requestMatchers("/resources/**", "/error");
+        return web -> web.ignoring().requestMatchers("/resources/**", "/error" , "/webjars/**");
     }
 
    /* @Bean
